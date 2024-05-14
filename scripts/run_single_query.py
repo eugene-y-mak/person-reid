2b48904fda95d6b4d87c2b62870f40fa8d0b0a63
Eugene Mak <yujinmak@gmail.com>
2024-05-14 16:39:44 -0400

    checked dist is relatively accurate measure

--- scripts/run_single_query.py
+++ scripts/run_single_query.py
@@ -1,6 +1,17 @@
 import torchreid
 from torchvision.io import read_image
 import torch
+import os
+import pickle
+import numpy as np
+
+def get_all_file_paths(directory):
+    file_paths = []
+    for root, _, files in os.walk(directory):
+        for file in files:
+            file_paths.append(os.path.join(root, file))
+    return file_paths
+
 
 def euclidean_squared_distance(input1, input2):
     """Computes euclidean squared distance.
@@ -20,13 +31,20 @@ def euclidean_squared_distance(input1, input2):
     return distmat
 
 class EngineRunner:
-    def __init__(self, query_path, gallery_path):
+    def __init__(self, query_path):
         self.model = None
         self.query_path = query_path
 
         # Should only be used for testing.
         # In practice, replace with on the fly image to test from YOLO
-        self.gallery_path = gallery_path
+        self.gallery_path = None
+
+    """
+    :param test_path: string path to new test image
+    """
+    def test_image(self, test_path):
+        self.gallery_path = test_path
+        return self.run()
 
     def initialize_model(self):
         MODEL_NAME = "osnet_x0_5"
@@ -49,7 +67,8 @@ class EngineRunner:
         return read_image(path)
 
     def extract_features(self, img):
-        return self.model(img.float())
+        with torch.no_grad():
+            return self.model(img.float())
 
     def run(self):
         self.initialize_model()
@@ -71,14 +90,33 @@ class EngineRunner:
         # 3.) compute distance. Before, was distance matrix, but also had many queries and galleries, so unnecessary.
         # try euclidean distance
         dist = euclidean_squared_distance(query_features, gallery_features)
-        print(dist)
+        dist = dist.numpy()
+        return dist[0][0]
         # correct: 22522.7500
         # wrong: 70822.5000
         # 4.) Somehow use distance value and determine if counts as a match or not
 
 
 if __name__ == "__main__":
+    THRESHOLD_DIST = 25000  # HIGHLY suspect prechosen hyperparameter, but dunno how else to tell
+    #directory = 'single_query_test/gallery'
+    directory = 'reid-data/grid/underground_reid/probe'
+    TEST_PATHS = get_all_file_paths(directory)
+    print(TEST_PATHS)
+
     QUERY_PATH = "single_query_test/query/probe.jpeg"
-    TEST_PATH = "single_query_test/gallery/wrong.jpeg"
-    engine_runner = EngineRunner(QUERY_PATH, TEST_PATH)
-    engine_runner.run()
+   # TEST_PATHS = ["single_query_test/gallery/correct.jpeg", "single_query_test/gallery/wrong.jpeg", "single_query_test/gallery/wrong2.png"]
+    dists = []
+    for i,test in enumerate(TEST_PATHS):
+        print(f"----------------------Image {i+1}-----------------------")
+        engine_runner = EngineRunner(QUERY_PATH)
+        dists.append(engine_runner.test_image(test))
+    print(dists)
+    dists = np.array(dists)
+    with open('dist.pkl', 'wb') as handle:
+        pickle.dump(dists, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    indices = np.where(dists < THRESHOLD_DIST)[0]
+    print(indices)
+    print(dists[indices])
+    matching_paths = np.array(TEST_PATHS)[indices]
+    print(matching_paths)
