973a9b93cd4c5566a9e55b44299a996fbb9f3eee
Eugene Mak <yujinmak@gmail.com>
2024-05-15 00:29:30 -0400

    added non torchreid method

---
+++ scripts/single_query.py
@@ -0,0 +1,190 @@
+import torchreid
+from torchvision.io import read_image, ImageReadMode
+import torch
+import os
+import pickle
+import cv2
+import numpy as np
+from torch.nn import functional as F
+import torchvision.transforms as transforms
+
+
+def get_all_file_paths(directory):
+    file_paths = []
+    for root, _, files in os.walk(directory):
+        for file in files:
+            file_paths.append(os.path.join(root, file))
+    return file_paths
+
+
+class SingleQueryModel:
+    def __init__(self, model_name, weight_path, query_path):
+        self.model_name = model_name
+        self.model = None
+        self.is_torchreid = True
+        self.weight_path = weight_path
+        self.query_path = query_path
+        self.euclidean = False
+        self.initialize_model()
+
+    """
+    :param test_path: string path to new test image
+    """
+
+    @staticmethod
+    def euclidean_squared_distance(input1, input2):
+        """Computes euclidean squared distance.
+
+        Args:
+            input1 (torch.Tensor): 2-D feature matrix.
+            input2 (torch.Tensor): 2-D feature matrix.
+
+        Returns:
+            torch.Tensor: distance matrix.
+        """
+        m, n = input1.size(0), input2.size(0)
+        mat1 = torch.pow(input1, 2).sum(dim=1, keepdim=True).expand(m, n)
+        mat2 = torch.pow(input2, 2).sum(dim=1, keepdim=True).expand(n, m).t()
+        distmat = mat1 + mat2
+        distmat.addmm_(input1, input2.t(), beta=1, alpha=-2)
+        return distmat
+
+    @staticmethod
+    def cosine_distance(input1, input2):
+        """Computes cosine distance.
+
+        Args:
+            input1 (torch.Tensor): 2-D feature matrix.
+            input2 (torch.Tensor): 2-D feature matrix.
+
+        Returns:
+            torch.Tensor: distance matrix.
+        """
+        input1_normed = F.normalize(input1, p=2, dim=1)
+        input2_normed = F.normalize(input2, p=2, dim=1)
+        distmat = 1 - torch.mm(input1_normed, input2_normed.t())
+        return distmat
+
+    @staticmethod
+    def simple_cosine(input1, input2):
+        return 1 - np.matmul(np.squeeze(input1), np.squeeze(input2).transpose())
+
+    def test_image(self, test_path):
+        query = self.load_image(self.query_path)
+        gallery = self.load_image(test_path)
+        return self.run(query, gallery)
+
+    def test_matrix(self, img_matrix):
+        query = self.load_image(self.query_path)
+        if not self.is_torchreid:
+            return self.run(query, np.array(img_matrix))
+        else:
+            transform = transforms.ToTensor()
+            test_tensor = transform(img_matrix)
+            return self.run(query, test_tensor)
+
+    def initialize_model(self):
+        if self.is_torchreid:
+            model = torchreid.models.build_model(
+                name=self.model_name,
+                num_classes=2,  # number of training identities, so might not matter
+                loss="softmax",
+                pretrained=True
+            )
+            torchreid.utils.load_pretrained_weights(model, self.weight_path)
+            model.eval()  # don't want to train
+            self.model = model
+        else:
+            self.model = cv2.dnn.readNet("pretrained/youtu_reid_baseline_lite.onnx")
+
+    def process(self, img):
+        if self.is_torchreid:
+            with torch.no_grad():
+                return self.model(img.float())
+        else:
+            print(img.shape)
+            blob = cv2.dnn.blobFromImage(img, 0.5, (128, 256), (128, 128, 128), False, False)
+            print(blob.shape)
+            self.model.setInput(blob)
+            res = self.model.forward()
+            print(res.shape)
+            return cv2.normalize(res, None)
+
+
+    """
+    :param path: string path to image
+    :return: Tensor[image_channels, image_height, image_width]
+    """
+
+    def load_image(self, path):
+        if not self.is_torchreid:
+            img = cv2.imread(path)
+            return cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        else:
+            return read_image(path, mode=ImageReadMode.RGB)  # force as RGB
+
+    def distance_metric(self, input1, input2):
+        if not self.is_torchreid:
+            return self.simple_cosine(input1, input2)
+        elif self.euclidean:
+            return self.euclidean_squared_distance(input1, input2)
+        else:
+            return self.cosine_distance(input1, input2)
+
+    def run(self, query, gallery):
+        # 1.) import query and gallery img as tensors
+        # add batch dimension (N=1)
+        if self.is_torchreid:
+            query = query.unsqueeze(0)
+            gallery = gallery.unsqueeze(0)
+
+        # 2.) run model on tensor inputs
+        query_features = self.process(query)
+        gallery_features = self.process(gallery)
+        # print(query_features)
+        # print(gallery_features)
+        # not sure what this does, but scared what happens if I don't
+        # query_features = query_features.cpu()
+        # gallery_features = gallery_features.cpu()
+        # 3.) compute distance. Before, was distance matrix, but also had many queries and galleries, so unnecessary.
+
+        dist = self.distance_metric(query_features, gallery_features)
+        if self.is_torchreid:
+            dist = dist.numpy()
+            dist = dist[0][0]
+        return dist
+        # correct: 22522.7500
+        # wrong: 70822.5000
+        # 4.) Somehow use distance value and determine if counts as a match or not
+
+
+if __name__ == "__main__":
+    # 0_5: [0.0072414875, 0.07200009, 0.05656421, 0.009953499, 0.08393031]
+    # 1_0: [0.046495795, 0.13216895, 0.41989136, 0.07390034, 0.48588467]
+    # ain 1_0: [0.024422288, 0.099429786, 0.18903148, 0.011394441, 0.56568325]
+    # ibn 1_0: [0.04042691, 0.57059103, 0.6661659, 0.3205316, 0.5463598]
+
+    MODEL = "osnet_x1_0"
+    directory = 'single_query_test/test'
+    #directory = 'reid-data/grid/underground_reid/probe'
+    TEST_PATHS = get_all_file_paths(directory)
+    WEIGHT_PATH = "pretrained/osnet_x1_0_imagenet.pth"
+    #QUERY_PATH = 'reid-data/grid/underground_reid/probe/0005_2_25100_229_94_99_249.jpeg'
+    QUERY_PATH = "single_query_test/test/nikita2.png"
+    dists = []
+    for i, test in enumerate(TEST_PATHS):
+        print(f"----------------------Image {i + 1}-----------------------")
+        engine_runner = SingleQueryModel(MODEL, WEIGHT_PATH, QUERY_PATH)
+        dists.append(engine_runner.test_image(test))
+    print(TEST_PATHS)
+    print(dists)
+    dists = np.array(dists)
+
+    THRESHOLD_DIST = 0.01  # HIGHLY suspect prechosen hyperparameter, but dunno how else to tell
+    indices = np.where(dists < THRESHOLD_DIST)[0]
+    print(indices)
+    print(dists[indices])
+    matching_paths = np.array(TEST_PATHS)[indices]
+    print(matching_paths)
+    # with open('dist.pkl', 'wb') as handle:
+    #     pickle.dump(dists, handle, protocol=pickle.HIGHEST_PROTOCOL)
---
+++ scripts/yolo_test.py
@@ -0,0 +1,80 @@
+from ultralytics import YOLO
+import cv2
+import math
+from single_query import SingleQueryModel
+
+# Change this depending on system (i.e. which port webcam is in). Probably 0 or 1
+CAMERA_NUM = 1
+# start webcam
+cap = cv2.VideoCapture(CAMERA_NUM)
+cap.set(3, 640)
+cap.set(4, 480)
+
+# model
+model = YOLO("yolo-Weights/yolov8n.pt")  # v8x
+classNames = ["Person"]
+
+# reid
+reid_model_name = "osnet_x0_25"
+reid_weights = "pretrained/osnet_x0_25_imagenet.pth"
+query_path = "query.jpg"
+sq_model = SingleQueryModel(reid_model_name, reid_weights, query_path)
+THRESHOLD = 0.73
+
+
+def main():
+    i = 0
+    box_color = (255, 0, 255)
+    while True:
+        i = i % 3
+        success, img = cap.read()
+        results = model(img, stream=True, classes=0, verbose=False)
+
+        # coordinates
+        for r in results:
+            boxes = r.boxes
+            for box in boxes:
+                # bounding box
+                x1, y1, x2, y2 = box.xyxy[0]
+                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)  # convert to int values
+
+                # do reid
+                if i == 0:
+                    subimage = img[y1:y2, x1:x2]
+                    dist = sq_model.test_matrix(subimage)
+                    print(f"=============================similarity: {dist}============================")
+                    if dist < THRESHOLD:
+                        box_color = (0, 255, 0)
+                    else:
+                        box_color = (255, 0, 255)
+                # put box in cam
+                cv2.rectangle(img, (x1, y1), (x2, y2), box_color, 3)
+
+                # confidence
+                confidence = math.ceil((box.conf[0] * 100)) / 100
+                # print("Confidence --->", confidence)
+
+                # class name
+                cls = int(box.cls[0])
+                # print("Class name -->", classNames[cls])
+
+                # object details
+                org = [x1, y1]
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                fontScale = 1
+                color = (255, 0, 0)
+                thickness = 2
+
+                cv2.putText(img, classNames[cls], org, font, fontScale, color, thickness)
+
+        cv2.imshow('Webcam', img)
+        if cv2.waitKey(1) == ord('q'):
+            break
+        i += 1
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
